a73be1db931b418d38ecfe10ce991aa1e0f67603
v_vzjyan <v_vzjyan@tencent.com>
2020-12-28 16:27:28 +0800

    v1.1-统一日志名字格式,日志按时间拆分

--- user/main.go
+++ user/main.go
@@ -1,24 +1,31 @@
 package main
 
-import "ylog"
+import (
+	"github.com/yanzijie/ylog"
+)
 
 var logger ylog.Logger
 
 func main(){
-	//logger := ylog.NewFileLogger(ylog.DebugLevel,"xxx.log","./")
-	//cb := "彩笔"
-	//defer logger.Close()
-	//logger.Debug("%s222是个好演员",cb)
-	//logger.Info("test2 Info")
-	//logger.Warn("test2 Warn")
-	//logger.Error("test2 error")
-	//logger.Fatal("test2 fatal")
-
+	//写入文件
 	//初始化时候设置最低打印级别，如果想打印全部级别，就设置最低的debugLevel级别就可以
-	cFile := ylog.NewConsoleLogger(ylog.DebugLevel)
-	cFile.Debug("DebugConsole")
-	cFile.Info("InfoConsole")
-	cFile.Warn("WarnConsole")
-	cFile.Error("ErrorConsole")
-	cFile.Fatal("FatalConsole")
+	//NewFileLogger()函数的第一个参数是打印级别，
+	//第二个参数日志存放路径（相对于程序可执行文件的路径）,建议写绝对路径，写到配置文件里面
+	logger := ylog.NewFileLogger(ylog.DebugLevel,"./")
+	cb := "彩笔"
+	defer logger.Close()
+	logger.Debug("%s222是个好演员",cb)
+	logger.Info("test2 Info")
+	logger.Warn("test2 Warn")
+	logger.Error("test2 error")
+	logger.Fatal("test2 fatal")
+
+	//输出到终端
+	//初始化时候设置最低打印级别，如果想打印全部级别，就设置最低的debugLevel级别就可以
+	//cFile := ylog.NewConsoleLogger(ylog.DebugLevel)
+	//cFile.Debug("DebugConsole")
+	//cFile.Info("InfoConsole")
+	//cFile.Warn("WarnConsole")
+	//cFile.Error("ErrorConsole")
+	//cFile.Fatal("FatalConsole")
 }
--- writeToFile.go
+++ writeToFile.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strconv"
 	"time"
 )
 
@@ -20,7 +21,9 @@ type FileLogger struct {
 }
 
 //日志文件结构构造函数
-func NewFileLogger(logLv level,fileName,filePath string)*FileLogger{
+func NewFileLogger(logLv level,filePath string)*FileLogger{
+	fileName := getFileName()
+	//名字统一格式，表示创建这个日志的时间，例如: 2020-10-12-13 - 2020年10月12日13点的日志
 	f := &FileLogger{
 		logLv:logLv,
 		fileName: fileName,
@@ -29,6 +32,47 @@ func NewFileLogger(logLv level,fileName,filePath string)*FileLogger{
 	}
 	f.initFile()	//初始化文件句柄
 	return f
+}
+
+func getFileName()(fileName string){
+	year := time.Now().Year()
+	month := time.Now().Month()
+	day := time.Now().Day()
+	hour := time.Now().Hour()
+
+	strYear := strconv.Itoa(year)
+	var strMonth string
+	switch month {
+	case time.January:
+		strMonth = "01"
+	case time.February:
+		strMonth = "02"
+	case time.March:
+		strMonth = "03"
+	case time.April:
+		strMonth = "04"
+	case time.May:
+		strMonth = "05"
+	case time.June:
+		strMonth = "06"
+	case time.July:
+		strMonth = "07"
+	case time.August:
+		strMonth = "08"
+	case time.September:
+		strMonth = "09"
+	case time.October:
+		strMonth = "10"
+	case time.November:
+		strMonth = "11"
+	case time.December:
+		strMonth = "12"
+	}
+	strDay := strconv.Itoa(day)
+	strHour := strconv.Itoa(hour)
+
+	fileName = strYear + "-" + strMonth + "-" + strDay + "-" + strHour + ".log"
+	return fileName
 }
 
 //打开指定日志文件，赋值给结构体字段
@@ -65,17 +109,69 @@ func (f *FileLogger)splitLogFile(file *os.File)*os.File{
 		panic(fmt.Errorf("open file %s error : %v",fileObj,err))
 	}
 	return fileObj
-
 }
 
 //判断文件大小，看一下是否要切分
 func (f *FileLogger)checkFileSize(file *os.File)bool{
 	fileInfo,_ := file.Stat()
+	//fmt.Println("Name ====",fileInfo.Name())  //这里得到的是文件的名字，不是路径+名字
 	fileSize := fileInfo.Size()
 	if fileSize >= f.maxSize{
 		return true		//要切分文件
 	}
 	return false	//不用切分文件
+}
+
+//判断文件时间，看一下是否要切分
+func (f *FileLogger)checkFileTime(file *os.File)bool{
+	fileInfo,_ := file.Stat()
+	newFileName := getFileName()
+	if fileInfo.Name() != newFileName{
+		return true //需要拆分
+	}
+	return false	//不用切分文件
+}
+func (f *FileLogger)checkFileTimeErr(file *os.File)bool{
+	fileInfo,_ := file.Stat()
+	newFileName := getFileName()
+	newFileName = newFileName + ".err"
+	if fileInfo.Name() != newFileName{
+		return true //需要拆分
+	}
+	return false	//不用切分文件
+}
+
+//按照时间切分文件,正常输出的log文件
+func (f *FileLogger)splitLogFileTime(file *os.File)*os.File{
+	//关闭原来的文件
+	_ = file.Close()
+
+	//1.拼接日志文件路径名字,打开新文件得到文件句柄
+	newFileName := getFileName()
+	logName := path.Join(f.filePath,newFileName)
+	fileObj,err := os.OpenFile(logName, os.O_CREATE|os.O_WRONLY|os.O_APPEND,0664)
+	if err!=nil{
+		panic(fmt.Errorf("open file %s error : %v",fileObj,err))
+	}
+
+	//返回新的
+	return fileObj
+}
+//按照时间切分文件,错误输出的log文件
+func (f *FileLogger)splitLogFileTimeErr(file *os.File)*os.File{
+	//关闭原来的文件
+	_ = file.Close()
+	//1.拼接日志文件路径名字,打开新文件得到文件句柄
+	newFileName := getFileName()
+	newFileName = newFileName + ".err"
+	logName := path.Join(f.filePath,newFileName)
+	fileObj,err := os.OpenFile(logName, os.O_CREATE|os.O_WRONLY|os.O_APPEND,0664)
+	if err!=nil{
+		panic(fmt.Errorf("open file %s error : %v",fileObj,err))
+	}
+
+	//返回新的
+	return fileObj
 }
 
 //把日志写入文件`	format日志内容，args-占位符的参数
@@ -91,20 +187,35 @@ func (f *FileLogger)writeContent(lv level,format string, args ...interface{}){
 	logLvStr := getLevelStr(lv)
 	logMsg := fmt.Sprintf("[%s][%s:%d][%s][%s]%s",nowTime,fileName,line,funcName,logLvStr,msg)
 
-	//判断文件大小
 	var res bool
-	res = f.checkFileSize(f.file)
-	if res == true{
-		f.file = f.splitLogFile(f.file)
-	}
+	//判断文件大小
+	//res = f.checkFileSize(f.file)
+	//if res == true{
+	//	f.file = f.splitLogFile(f.file)
+	//}
+	////写入文件
+	//_,_ = fmt.Fprintln(f.file,logMsg)
 
+	//判断时间,看看需不需要拆分, 这里判断的是正常的log名字
+	res = f.checkFileTime(f.file)
+	if res == true{
+		f.file = f.splitLogFileTime(f.file)
+	}
 	//写入文件
 	_,_ = fmt.Fprintln(f.file,logMsg)
+
 	//如果是错误或者崩溃的日志，还要多记录进错误日志文件
 	if lv >= ErrorLevel{
-		res = f.checkFileSize(f.errFile)
+		//res = f.checkFileSize(f.errFile)
+		//if res == true{
+		//	f.errFile = f.splitLogFile(f.errFile)
+		//}
+		//_,_ = fmt.Fprintln(f.errFile,logMsg)
+
+		//这里判断的是err的log名字
+		res = f.checkFileTimeErr(f.errFile)
 		if res == true{
-			f.errFile = f.splitLogFile(f.errFile)
+			f.errFile = f.splitLogFileTimeErr(f.errFile)
 		}
 		_,_ = fmt.Fprintln(f.errFile,logMsg)
 	}
